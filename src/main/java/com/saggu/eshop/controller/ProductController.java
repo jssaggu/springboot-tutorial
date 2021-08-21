88998b7a2545575347284cc9f1a0e565c95d7f13
Jasvinder S Saggu <jasvinder.saggu@mastercard.com>
2021-08-21 21:38:20 +0100

    Open API and Swagger Implementation

--- src/main/java/com/saggu/eshop/controller/ProductController.java
+++ src/main/java/com/saggu/eshop/controller/ProductController.java
@@ -2,16 +2,25 @@ package com.saggu.eshop.controller;
 
 import com.saggu.eshop.dao.ProductDao;
 import com.saggu.eshop.dto.ProductDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @RestController
+@RequestMapping("v1/")
 public class ProductController {
 
     private final ProductDao productDao;
@@ -20,7 +29,14 @@ public class ProductController {
         this.productDao = productDao;
     }
 
-    @GetMapping(value = "/products")
+    @Operation(summary = "Get products", description = "Get a list of Products", tags = "Get")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the Product",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ProductDto.class))}),
+            @ApiResponse(responseCode = "404", description = "Products not found",
+                    content = @Content)})
+    @GetMapping(value = "/products", produces = APPLICATION_JSON_VALUE)
     public List<ProductDto> products() {
         return productDao.getProducts();
     }
