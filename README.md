f0b9f2d72c8ac66cb891624756e16d4027587037
jasvinder-saggu <jasvinder.saggu@mastercard.com>
2022-01-16 22:37:27 +0000

    Added github actions

--- README.md
+++ README.md
@@ -1,29 +1,56 @@
 # SpringBoot Tutorial
+![Java Maven CI](https://github.com/jssaggu/springboot-tutorial/actions/workflows/maven.yml/badge.svg)
 
 ## Videos Available here
+All the tutorial videos are available on Saggu.uk YouTube channel.
 
-https://www.youtube.com/playlist?list=PLYwGWvgqiQCkgxraQU-fuOiYzNCcjGAlz
+[![Watch the video](docs/Spring-Tutorial-Playlist.png)](https://www.youtube.com/playlist?list=PLYwGWvgqiQCkgxraQU-fuOiYzNCcjGAlz)
 
 ## Spring Profiles
 
 #### How to run using a profile?
 
-`mvn spring-boot:run -DskipTests -Dspring-boot.run.profiles={Profile-Name}`
+```
+mvn spring-boot:run -DskipTests -Dspring-boot.run.profiles={Profile-Name}
+```
 
 e.g. For Dev
 
-```mvn spring-boot:run -DskipTests -Dspring-boot.run.profiles={Profile-Name}```
+```
+mvn spring-boot:run -DskipTests -Dspring-boot.run.profiles={Profile-Name}
+```
 
 e.g. Different Port
 
-```mvn spring-boot:run -Dspring-boot.run.arguments=--server.port=8080```
-```mvn spring-boot:run -Dspring-boot.run.arguments=--server.port=8090```
+```
+mvn spring-boot:run -Dspring-boot.run.arguments=--server.port=8080
+mvn spring-boot:run -Dspring-boot.run.arguments=--server.port=8090
+```
+
+or
+
+```
+mvn clean install -DskipTests
+java -jar target/springboot-tutorial-0.0.1-SNAPSHOT.jar --server.port=8080 --management.server.port=9090
+```
 
 ## Spring Caching
 
-### How to use Redis?
+*Note:* Just enable one cache provider
 
-Update the following in application.yml
+### Redis
+
+###### Dependency
+```xml
+<dependency>
+    <groupId>org.springframework.boot</groupId>
+    <artifactId>spring-boot-starter-data-redis</artifactId>
+    <version>${cache.version}</version>
+</dependency>
+```
+
+###### application.yml
+Add the following: 
 ```yaml
 spring:
   cache:
@@ -32,7 +59,27 @@ spring:
     type: redis
 ```
 
-#### How to use install Redis using Docker?
+###### How to install Redis using Docker?
 
 * docker pull redis
 * docker run --name saggu.uk -p 6379:6379 -d redis
+
+### Hazelcast
+
+###### Dependency
+```xml
+<dependency>
+    <groupId>com.hazelcast</groupId>
+    <artifactId>hazelcast-all</artifactId>
+    <version>4.2.4</version>
+</dependency>
+```
+
+###### Add hazelcast.yaml
+```yaml
+hazelcast:
+  network:
+    join:
+      multicast:
+        enabled: true
+```
--- src/main/java/com/saggu/eshop/dao/OrderDao.java
+++ src/main/java/com/saggu/eshop/dao/OrderDao.java
@@ -3,22 +3,19 @@ package com.saggu.eshop.dao;
 import com.saggu.eshop.dto.ProductDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springdoc.api.OpenApiResourceNotFoundException;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
+import org.webjars.NotFoundException;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
-
 @Repository
 @Slf4j
 public class OrderDao {
 
-    private static final Map<String, ProductDto> orders = new HashMap<>();
+    private static final Map<String, String> orders = new HashMap<>();
     private static AtomicInteger ORDER_ID = new AtomicInteger(100);
     private final ProductDao productDao;
 
@@ -28,34 +25,35 @@ public class OrderDao {
 
     public Map<String, ProductDto> getOrders() throws InterruptedException {
         log.info("Calling service to get Orders data...");
-//        SECONDS.sleep(1);
-        return orders;
+        Map<String, ProductDto> ordersWithDto = new HashMap<>();
+        for (Map.Entry<String, String> entry : orders.entrySet()) {
+            productDao.getProduct(entry.getValue())
+                    .ifPresent(p -> ordersWithDto.put(entry.getKey(), p));
+        }
+        return ordersWithDto;
     }
 
-    @Cacheable(value = "orders", key = "#orderId")
     public ProductDto getOrder(String orderId) throws InterruptedException {
         log.info("Calling service to get Orders data...");
-        SECONDS.sleep(5);
-        return orders.get(orderId);
+        return productDao.getProduct(orders.get(orderId))
+                .orElseThrow(() -> new NotFoundException("OrderId not found: " + orderId));
     }
 
-    @CachePut(value = "orders")
     public String addOrder(String productId) {
         String orderId = "O" + ORDER_ID.getAndIncrement();
         Optional<ProductDto> product = productDao.getProduct(productId);
         if (product.isPresent()) {
-            orders.put(orderId, product.get());
+            orders.put(orderId, product.get().getProductId());
             return orderId;
         } else {
-            throw new OpenApiResourceNotFoundException("Invalid Product ID: " + productId);
+            throw new NotFoundException("Invalid Product ID: " + productId);
         }
     }
 
-    @CachePut(value = "orders", key = "#orderId")
     public ProductDto updateOrder(String orderId, String productId) {
         Optional<ProductDto> product = productDao.getProduct(productId);
         if (product.isPresent()) {
-            orders.put(orderId, product.get());
+            orders.put(orderId, product.get().getProductId());
             return product.get();
         } else {
             throw new OpenApiResourceNotFoundException("Invalid Product ID: " + productId);
